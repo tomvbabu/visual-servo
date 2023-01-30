a6b4f3f4cca084c91365a01473e85767b6b987db
cap-code <advaith007cube@gmail.com>
2023-01-30 17:51:55 +0530

    imporved controls

---
+++ MasterN/MasterN.ino
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ef49f60e2f05f1ef61392c934a4c38cd42e4489a02994f7843c20f33b4b2321
+size 1675
---
+++ slaveN/slaveN.ino
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:108caa9318947025dcd9ce9f3f9524e7db0cdc574e65cf5c37dcc32b168549b4
+size 4245
