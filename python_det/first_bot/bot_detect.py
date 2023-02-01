95c00a15290435903c9fc057a8c681ee7dcd237d
Tom <tomvbabu@gmail.com>
2023-02-01 07:45:13 +0530

    new detection folder + artag

---
+++ python_det/first_bot/bot_detect.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7cc2926c563379203c136bee1bc7b3834219ff8d159e582140d904475d736b07
+size 5477
---
+++ python_det/first_bot/bot_square.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:af0620636dce167313e7b796074254bd8b5b250e4f777865c9006121c487f2b6
+size 5135
