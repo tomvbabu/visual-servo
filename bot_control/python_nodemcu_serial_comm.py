948ab0a89f56a2c3180112a08d8b7971222e8559
cap-code <advaith007cube@gmail.com>
2023-02-02 12:35:14 +0530

    improved bot control v2

---
+++ bot_control/python_nodemcu_serial_comm.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f3152037e42d8e5bfe627f593bac62354713efc72d2b6ba8fb958dd7c8d382a2
+size 423
---
+++ bot_control/slaveN/slaveN.ino
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:742454ca841a718b386d974213da89c223ac9a541900d65d7b5e9e9eba93f199
+size 3096
