1fde6e93b9a14c2be0bac000c2752ac4a0fa6ff1
Tom <tomvbabu@gmail.com>
2023-02-07 12:22:56 +0530

    comments and aruco marker angle included

--- python_det/first_bot/bot_detect.py
+++ python_det/first_bot/bot_detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b966709f7c7357e080833eec1136b94a90afa08f0480e1e809a08c8be848aa8e
-size 7495
+oid sha256:fa5d0799f102d7ad80088b1467a3947510d1d3999e18da9c45f425f2b6142db9
+size 10931
