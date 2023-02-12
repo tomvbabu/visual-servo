92f16626d81d145491c29e869f3c202a8a7c0695
Tom <tomvbabu@gmail.com>
2023-02-12 20:42:11 +0530

    collision added

--- python_det/first_bot/bot_detect.py
+++ python_det/first_bot/bot_detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa5d0799f102d7ad80088b1467a3947510d1d3999e18da9c45f425f2b6142db9
-size 10931
+oid sha256:677267a777bea17cb5da62fa8c7edf16cddeef1f2fad28099689340f151b3535
+size 13422
